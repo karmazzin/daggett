5a2cf4fd9a5eafc13cc7c16446ac7e502e1aa664
Anton Karmazin <karmazzin@ya.ru>
2016-04-27 12:39:21 +0300

    Bot initial code

---
+++ index.js
@@ -0,0 +1,19 @@
+var Botkit = require('botkit');
+
+var controller = Botkit.slackbot({
+  debug: false
+  //include "log: false" to disable logging
+  //or a "logLevel" integer from 0 to 7 to adjust logging verbosity
+});
+
+// connect the bot to a stream of messages
+controller.spawn({
+  token: 'xoxb-37982075332-ZQ3BakzlK4BJx5jDze1PsOLO',
+}).startRTM()
+
+// give the bot something to listen for.
+controller.hears('hello',['direct_message','direct_mention','mention'],function(bot,message) {
+
+  bot.reply(message,'Hello yourself.');
+
+});
