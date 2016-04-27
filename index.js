fa8700e09983eccc8d6b6cba3911cd16b8a507a9
Anton Karmazin <karmazzin@ya.ru>
2016-04-27 13:23:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,3 +15,31 @@ controller.spawn({
 controller.hears('hello',['direct_message','direct_mention','mention'],function(bot,message) {
   bot.reply(message,'Hello yourself.');
 });
+
+controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name', 'ку-ку, ёпта', 'ку-ку, епта',],
+    'direct_message,direct_mention,mention', function(bot, message) {
+        var hostname = os.hostname();
+        var uptime = formatUptime(process.uptime());
+
+        bot.reply(message,
+            ':robot_face: I am a bot named <@' + bot.identity.name +
+             '>. I have been running for ' + uptime + ' on ' + hostname + '.');
+    });
+
+function formatUptime(uptime) {
+    var unit = 'second';
+    if (uptime > 60) {
+        uptime = uptime / 60;
+        unit = 'minute';
+    }
+    if (uptime > 60) {
+        uptime = uptime / 60;
+        unit = 'hour';
+    }
+    if (uptime != 1) {
+        unit = unit + 's';
+    }
+
+    uptime = uptime + ' ' + unit;
+    return uptime;
+}
